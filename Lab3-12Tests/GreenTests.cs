e57cac71f92ef17debd758d2d27f0eff9c93dc5d
даниил дикальчук <sana.dana.81@hotmail.com>
2022-11-07 13:01:10 +0800

    Создал автотест для сложения зелёного

---
+++ Lab3-12Tests/GreenTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab3_12;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_12.Tests
+{
+    [TestClass()]
+    public class GreenTests
+    {
+        [TestMethod()]
+        public void GreenSumTest()
+        {
+            var rgb = new RGB(0, 133, 255);
+            var g = new Green(133);
+            rgb += g;
+            Assert.AreEqual(133, rgb.G());
+        }
+    }
+}
