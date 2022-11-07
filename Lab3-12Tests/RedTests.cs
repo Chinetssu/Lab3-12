e2c5250dac9fbdf2f97d30770a3a595c2ea2bb59
даниил дикальчук <sana.dana.81@hotmail.com>
2022-11-07 12:58:41 +0800

    Создал тест для сложения крассного цвета

---
+++ Lab3-12Tests/RedTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab3_12;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_12.Tests
+{
+    [TestClass()]
+    public class RedTests
+    {
+        [TestMethod()]
+        public void RedSumTest()
+        {
+            var rgb = new RGB(0, 65, 255);
+            var r = new Red(255);
+            rgb += r;
+            Assert.AreEqual(127, rgb.R());
+        }
+    }
+}
