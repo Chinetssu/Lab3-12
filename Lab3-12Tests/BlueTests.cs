0654c5c58761483221e3263000b12751e94c1beb
даниил дикальчук <sana.dana.81@hotmail.com>
2022-11-07 13:02:18 +0800

    Создал автотест для сложения синего оттенка

---
+++ Lab3-12Tests/BlueTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab3_12;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_12.Tests
+{
+    [TestClass()]
+    public class BlueTests
+    {
+        [TestMethod()]
+        public void BlueTest()
+        {
+            var rgb = new RGB(0, 65, 255);
+            var b = new Blue(255);
+            rgb += b;
+            Assert.AreEqual(255, rgb.B());
+        }
+    }
+}
